a68c4d83ca34d53b0cd9659b30f21a7d035dec28
Misha Serbenyuk <80031477+MishaSerbenyuk@users.noreply.github.com>
2021-03-23 14:00:48 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
 echo "Running Magento2 PHP CodeSniffer"
-composer require --dev magento/magento-coding-standard
-vendor/bin/phpcs --config-set installed_paths ../../magento/magento-coding-standard/
+composer global require --dev magento/magento-coding-standard
+vendor/bin/phpcs --config-set installed_paths ~/.composer/vendor/magento/magento-coding-standard/
 vendor/bin/phpcs --ignore=*/vendor/* --standard=Magento2 ./
